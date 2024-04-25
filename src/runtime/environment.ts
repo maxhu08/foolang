40967f4d1378fcb1c18aa35b86bedef37b57d471
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-04-25 18:40:20 -0500

    environment stuff

--- src/runtime/environment.ts
+++ src/runtime/environment.ts
@@ -2,10 +2,44 @@ import type { RuntimeValue } from "./values";
 
 export default class Environment {
   private parent?: Environment;
-  private variables?: Map<string, RuntimeValue>;
+  private variables: Map<string, RuntimeValue>;
 
   constructor(parentENV?: Environment) {
     this.parent = parentENV;
     this.variables = new Map();
+  }
+
+  public declareVariable(varname: string, value: RuntimeValue): RuntimeValue {
+    if (this.variables.has(varname)) {
+      throw `Cannot redeclare existing variable: ${varname}`;
+    }
+
+    this.variables.set(varname, value);
+    return value;
+  }
+
+  public assignVariable(varname: string, value: RuntimeValue): RuntimeValue {
+    const env = this.resolve(varname);
+    env.variables.set(varname, value);
+
+    return value;
+  }
+
+  public lookupVariable(varname: string): RuntimeValue {
+    const env = this.resolve(varname);
+
+    return env.variables.get(varname) as RuntimeValue;
+  }
+
+  public resolve(varname: string): Environment {
+    if (this.variables.has(varname)) {
+      return this;
+    }
+
+    if (this.parent === undefined) {
+      throw `Cannot resolve: '${varname}' as it does not exist`;
+    }
+
+    return this.parent.resolve(varname);
   }
 }
